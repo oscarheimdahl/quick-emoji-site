528d77e02f8f820dbb99eba5fbdc3e057b8a4d5f
oscarheimdahl <o.heimdahl@gmail.com>
2021-03-18 21:35:10 +0100

    init

---
+++ src/app/app-view/app-view.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-view',
+  templateUrl: './app-view.component.html',
+  styleUrls: ['./app-view.component.scss'],
+})
+export class AppViewComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
