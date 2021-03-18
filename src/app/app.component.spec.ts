c4e1ee03f758081e622ad2296652a1d3e4ce56e0
oscarheimdahl <o.heimdahl@gmail.com>
2021-03-18 21:53:48 +0100

    dploy  ready

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'quick-emoji'`, () => {
+  it(`should have as title 'quick-emoji-site'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('quick-emoji');
+    expect(app.title).toEqual('quick-emoji-site');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('quick-emoji app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'quick-emoji-site app is running!'
+    );
   });
 });
